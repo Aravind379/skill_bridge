48d58e8b2755667d7a9fb1fcb15380f8e5c5ac55
aravind <varaganiaravind@gmail.com>
2026-01-02 14:57:02 +0530

    Day 2: Real skill gap API using admin data

--- backend/skills/views.py
+++ backend/skills/views.py
@@ -1,20 +1,31 @@
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import UserSkill
 
+@login_required
 def skill_gap(request):
-    data = [
-        {
-            "skill": "Python",
-            "current_level": 2,
-            "required_level": 4,
-            "gap": 2,
-            "severity": "High"
-        },
-        {
-            "skill": "SQL",
-            "current_level": 1,
-            "required_level": 3,
-            "gap": 2,
-            "severity": "Medium"
-        }
-    ]
-    return JsonResponse(data, safe=False)
+    user = request.user
+    user_skills = UserSkill.objects.filter(user=request.user)
+
+    response = []
+
+    for us in user_skills:
+        required_level = 4  # fixed for now
+        gap = required_level - us.current_level
+
+        if gap >= 3:
+            severity = "High"
+        elif gap == 2:
+            severity = "Medium"
+        else:
+            severity = "Low"
+
+        response.append({
+            "skill": us.skill.name,
+            "current_level": us.current_level,
+            "required_level": required_level,
+            "gap": gap,
+            "severity": severity
+        })
+
+    return JsonResponse(response, safe=False)
